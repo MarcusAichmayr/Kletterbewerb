49fba2f3a5e14d97539c728e22a5b352cc58c440
greenp42 <paul.greenlinger@gmail.com>
2024-04-01 23:12:50 +0200

    add col Ergebnis and line under headings

--- gui/group_box_group.py
+++ gui/group_box_group.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QGridLayout, QGroupBox, QHBoxLayout, QLabel, QSpinBox
+from PySide6.QtWidgets import QFrame, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QSpinBox
 
 from classes import Participant, Route
 
@@ -11,6 +11,7 @@ class GroupBoxGroup(QGroupBox):
     grid_layout: QGridLayout
     heading_labels: list[QLabel]
     participant_labels: dict[Participant, QLabel]
+    participant_res_labels: dict[Participant, QLabel]
     participant_try_inputs: dict[Participant, dict[Route, list[QSpinBox]]]
 
     def __init__(self, parent=None):
@@ -34,18 +35,28 @@ class GroupBoxGroup(QGroupBox):
         self.setTitle(grp_name)
         self.setup_header()
         self.participant_labels = {}
+        self.participant_res_labels = {}
         self.participant_try_inputs = {}
         for p in grp_participants:
             self.setup_participant(p)
 
     def setup_header(self) -> None:
         self.heading_labels = []
+
         col = 0
         lbl = QLabel(self)
         lbl.setObjectName(f"lbl_head_{col}")
         lbl.setText("Name")
         self.grid_layout.addWidget(lbl, 0, col, 1, 1)
         self.heading_labels.append(lbl)
+
+        col = 1
+        lbl = QLabel(self)
+        lbl.setObjectName(f"lbl_head_{col}")
+        lbl.setText("Ergebnis")
+        self.grid_layout.addWidget(lbl, 0, col, 1, 1)
+        self.heading_labels.append(lbl)
+
         for r in self.routes:
             col += 1
             lbl = QLabel(self)
@@ -56,6 +67,13 @@ class GroupBoxGroup(QGroupBox):
             self.heading_labels.append(lbl)
             # maybe add heading like "Versuch 1, Versuch 2, ..."
 
+        line = QFrame(self)
+        line.setObjectName("line")
+        line.setFrameShape(QFrame.Shape.HLine)
+        line.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.grid_layout.addWidget(line, 1, 0, 1, self.grid_layout.columnCount())
+
     def setup_participant(self, participant: Participant) -> None:
         """add an participant to the group box
 
@@ -65,19 +83,27 @@ class GroupBoxGroup(QGroupBox):
         row = self.grid_layout.rowCount() + 1
         col = 0
         lbl = QLabel(self)
-        lbl.setObjectName(f"lbl_participant_{self.name}_{col}")
+        lbl.setObjectName(f"lbl_participant_{self.name}_{row}_{col}")
         lbl.setText(participant.name)
         self.grid_layout.addWidget(lbl, row, col, 1, 1)
         self.participant_labels[participant] = lbl
+
+        col = 1
+        lbl = QLabel(self)
+        lbl.setObjectName(f"lbl_participant_{self.name}_{row}_{col}")
+        lbl.setText(str(participant.result))
+        self.grid_layout.addWidget(lbl, row, col, 1, 1)
+        self.participant_res_labels[participant] = lbl
+
         self.participant_try_inputs[participant] = {}
         for r in self.routes:
             col += 1
             tries_layout = QHBoxLayout()
-            tries_layout.setObjectName(f"tries_layout_{participant.name}_{col}")
+            tries_layout.setObjectName(f"tries_layout_{participant.name}_{row}_{col}")
             spin_boxes = []
             for t in range(self.tries_per_route):
                 spin_box = QSpinBox(self)
-                spin_box.setObjectName(f"tries_spin_box_{participant.name}_{col}_{t}")
+                spin_box.setObjectName(f"tries_spin_box_{participant.name}_{row}_{col}_{t}")
                 spin_boxes.append(spin_box)
                 tries_layout.addWidget(spin_box)
             self.participant_try_inputs[participant][r] = spin_boxes
