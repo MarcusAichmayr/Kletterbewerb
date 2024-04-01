9322d8fcb149f119c197da64e27a81667266641f
greenp42 <paul.greenlinger@gmail.com>
2024-04-01 20:55:10 +0200

    build group boxes with variable data

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,9 +1,33 @@
 from PySide6.QtWidgets import QMainWindow
 
+from classes import Group, Participant, Route
 from gui.ui_main_window import Ui_MainWindow
+from gui.group_box_group import GroupBoxGroup
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    groupBox: GroupBoxGroup
+    groupBox_2: GroupBoxGroup
+    groupBox_3: GroupBoxGroup
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+    def setup_groups(
+        self,
+        participants: list[Participant],
+        tries_per_route: int,
+        groups: list[Group],
+    ) -> None:
+        assert len(groups) == 3
+        boxes = {
+            groups[0]: self.groupBox,
+            groups[1]: self.groupBox_2,
+            groups[2]: self.groupBox_3,
+        }
+        for grp in groups:
+            box = boxes[grp]
+            grp_participants = [x for x in participants if x.group == grp]
+            # grp_routes = [x for x in routes if grp in x.groups]
+            box.setup_group(grp_participants, tries_per_route, grp.name, grp.routes)
--- ui_test.py
+++ ui_test.py
@@ -2,11 +2,14 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
+from classes import TRY_WEIGHTS
 from gui.main_window import MainWindow
+from set_data import groups, participants
 
 def main():
     app = QApplication(sys.argv)
     main_win = MainWindow()
+    main_win.setup_groups(participants, len(TRY_WEIGHTS), groups)
     main_win.show()
     sys.exit(app.exec())
 
